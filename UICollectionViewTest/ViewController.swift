c287a361477397c077c3124cb6d32efaf625c06e
harutiro <harutiro2027@outlook.jp>
2023-02-22 16:57:45 +0900

    Initial Commit

---
+++ UICollectionViewTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICollectionViewTest
+//
+//  Created by haruto.makino on 2023/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
