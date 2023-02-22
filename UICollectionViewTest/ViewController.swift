2344821d7dcba1da1e625974fc7d3aa78a6af708
harutiro <harutiro2027@outlook.jp>
2023-02-22 17:07:05 +0900

    add: List表示をできるように変更した。

--- UICollectionViewTest/ViewController.swift
+++ UICollectionViewTest/ViewController.swift
@@ -13,7 +13,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
 
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .blue
+        
+        return cell
+    }
+}
